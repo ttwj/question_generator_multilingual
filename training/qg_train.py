87b55c6587ae08c8d8f8288bdcbefeba824d3a1d
Terence Tan <terencetanwj@gmail.com>
2022-08-06 19:59:48 +0800

    Attempt to train on mt5 with Chinese dataset

--- training/qg_train.py
+++ training/qg_train.py
@@ -1,38 +1,41 @@
 import argparse
 import datasets
-from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer
+#from transformers import T5Config, T5ForConditionalGeneration, T5Tokenizer
+from transformers import MT5Config, MT5ForConditionalGeneration, MT5Tokenizer
 
 from dataset import QGDataset
 from trainer import Trainer
+
+from datasets import load_dataset
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataloader_workers", type=int, default=2)
-    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--device", type=str, default="cpu")
     parser.add_argument("--epochs", type=int, default=20)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
     parser.add_argument("--max_length", type=int, default=512)
-    parser.add_argument("--qg_model", type=str, default="t5-base")
+    parser.add_argument("--qg_model", type=str, default="google/mt5-small")
     parser.add_argument("--pad_mask_id", type=int, default=-100)
     parser.add_argument("--pin_memory", dest="pin_memory", action="store_true", default=False)
-    parser.add_argument("--save_dir", type=str, default="./t5-base-question-generator")
+    parser.add_argument("--save_dir", type=str, default="./mt5-base-question-generator")
     parser.add_argument("--train_batch_size", type=int, default=4)
     parser.add_argument("--valid_batch_size", type=int, default=32)
     return parser.parse_args()
 
 
-def get_tokenizer(checkpoint: str) -> T5Tokenizer:
-    tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+def get_tokenizer(checkpoint: str) -> MT5Tokenizer:
+    tokenizer = MT5Tokenizer.from_pretrained(checkpoint)
     tokenizer.add_special_tokens(
         {'additional_special_tokens': ['<answer>', '<context>']}
     )
     return tokenizer
 
 
-def get_model(checkpoint: str, device: str, tokenizer: T5Tokenizer) -> T5ForConditionalGeneration:
-    config = T5Config(decoder_start_token_id=tokenizer.pad_token_id)
-    model = T5ForConditionalGeneration(config).from_pretrained(checkpoint)
+def get_model(checkpoint: str, device: str, tokenizer: MT5Tokenizer) -> MT5ForConditionalGeneration:
+    config = MT5Config(decoder_start_token_id=tokenizer.pad_token_id)
+    model = MT5ForConditionalGeneration(config).from_pretrained(checkpoint)
     model.resize_token_embeddings(len(tokenizer))
     model = model.to(device)
     return model
@@ -41,9 +44,26 @@ def get_model(checkpoint: str, device: str, tokenizer: T5Tokenizer) -> T5ForCond
 if __name__ == "__main__":
     args = parse_args()
     tokenizer = get_tokenizer(args.qg_model)
-    dataset = datasets.load_dataset("iarfmoose/question_generator")
-    train_set = QGDataset(dataset["train"], args.max_length, args.pad_mask_id, tokenizer)
-    valid_set = QGDataset(dataset["validation"], args.max_length, args.pad_mask_id, tokenizer)
+    #dataset = datasets.load_dataset("iarfmoose/question_generator")
+    #print(dataset['train'])
+    #print(dataset['validation'])
+    #val_ds = datasets.load_dataset("bookcorpus", split=[f"train[{k}%:{k+10}%]" for k in range(0, 100, 10)])
+    #train_ds = datasets.load_dataset("bookcorpus", split=[f"train[:{k}%]+train[{k+10}%:]" for k in range(0, 100, 10)])
+    #dataset = load_dataset("csv", data_files="data_file.csv")
+    dataset_train = load_dataset("csv", data_files="data_file.csv", split=[f"train[:{k}%]+train[{k+10}%:]" for k in range(0, 100, 10)])
+    dataset_validation = load_dataset("csv", data_files="data_file.csv", split=[f"train[{k}%:{k+10}%]" for k in range(0, 100, 10)])
+
+    #train_set = QGDataset(dataset["train"], args.max_length, args.pad_mask_id, tokenizer)
+    #valid_set = QGDataset(dataset["validation"], args.max_length, args.pad_mask_id, tokenizer)
+
+    
+    train_set = QGDataset(dataset_train, args.max_length, args.pad_mask_id, tokenizer)
+    valid_set = QGDataset(dataset_validation, args.max_length, args.pad_mask_id, tokenizer)
+    
+    print(train_set)
+
+    print(valid_set)
+    
     model = get_model(args.qg_model, args.device, tokenizer)
     trainer = Trainer(
         dataloader_workers=args.dataloader_workers,
